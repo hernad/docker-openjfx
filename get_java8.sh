e1083e5ec0f3ae52f1aa25dfebdf60e34e34798e
Ernad Husremovic <hernad@bring.out.ba>
2015-12-15 22:46:52 +0100

    java8

--- get_java8.sh
+++ get_java8.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+VER=8.0.66
+
 docker rm openjfx
 docker run --name openjfx openjfx bash -c "cd /usr/lib/jvm/java-8-openjdk-amd64 && tar cvfz /java.tar.gz ."
 
-[ ! -d openjdk-8 ] && mkdir openjdk-8
+[ ! -d java8 ] && mkdir java8
 docker cp openjfx:/java.tar.gz openjdk-8/java.tar.gz
-cd openjdk-8
+cd java8
 tar xvf java.tar.gz
 rm java.tar.gz
 rm -r -f docs
 cd ..
 
-tar -cvzf openjdk-8.tar.gz openjdk-8
+tar -cvzf java8_${VER}.tar.gz java8
